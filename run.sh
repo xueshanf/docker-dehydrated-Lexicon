1b0715133bf8b5ac1c4c1482960d77a71208c493
mlaihk <marshall.lai@outlook.com>
2017-02-27 14:54:45 +0800

    Modify to create domains.txt if not exist

--- run.sh
+++ run.sh
@@ -7,7 +7,15 @@ interrupt() {
 }
 
 trap interrupt SIGINT
+
+#Accept terms on restart
 ./dehydrated --register --accept-terms
+
+#Create domains.txt file if not exist.
+if [ ! -f /letsencrypt/domains.txt ]; then
+   echo "www.test.domain.com" > /letsencrypt/domains.txt
+fi
+
 while true; do
   ./dehydrated --cron --hook /dns/hook --challenge dns-01
   ./dehydrated --cleanup
